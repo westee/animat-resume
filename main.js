ae77e0f2ef1a7157431edaead8d83cec580c6749
dflk <904885508@qq.com>
2018-08-11 11:41:52 +0800

    完成

--- main.js
+++ main.js
@@ -1,43 +1,8 @@
-//遇到的问题  
-//代码很长后不会自动向下滚动
 //把code写道 #code和 style 标签里
-function writeCode(prefix, code, fn) {
-  let domCode = document.querySelector('#code');
-  // domCode.innerHTML = prefix || ''
-  let n = 0;
-  let id = setInterval(() => {
-    n += 1;
-    // domCode.innerHTML = code.substring(0, n);
-    //将code的0到前n个字符高亮放到domCode中
-    domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
-    //将code的0到n个字符写到style标签中
-    styleTag.innerHTML = prefix + code.substring(0, n);
-    domCode.scrollTop = domCode.scrollHeight; //如果页面过长,就自动下拉,最多10000 
-    if (n >= code.length) {
-      window.clearInterval(id)
-      fn()   //fn && fn.call()
-    }
-  }, 5);
-}
-
-function writeMD(md, fn) {
-  let domPaper = document.querySelector('#paper>.content')
-  let n = 0;
-  let id = setInterval(() => {
-    n += 1;
-    //将code的0到前n个字符高亮放到domCode中
-    domPaper.innerHTML = md.substring(0, n);
-    domPaper.scrollTop = domPaper.scrollHeight; //如果页面过长,就自动下拉,最多10000 
-    if (n >= md.length) {
-      window.clearInterval(id)
-      fn()       //fn && fn.call()
-    }
-  }, 5);
-}
-
-let result = `
+ 
+var  result = `
  /*
-  *面试官你好,我是XXX
+  *面试官你好,我是某某某
   *我将以动画的形式来介绍我自己
 
   *只用文字介绍太单调了
@@ -73,52 +38,106 @@ let result = `
     transform:skew(4deg, 4deg);
   }
   /* 接下来就是自我介绍的时候了 */
-  /* 我需要一张白纸 */
-  #code{
-    position: fixed;
-    left: 0;
-    width: 50%;
-    height: 100%;
-  } 
-  #paper > .content{
-    background: lightblue;   
-    height: 100%;
-    width: 100%;
-  }
-  #paper{
-    position: fixed;
-    right: 0;
-    width: 50%;
-    height: 100%;
-    background: #ddd;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  /* 我需要一张纸 */
 
+  #code-wrapper{
+    width: 50%; left: 0; position: fixed; 
+    height: 100%;
+  }
+  #paper > .content {
+   display: block;
+  }
   `
-let result2 = `
+
+var result2 = `
+接下来用Marked.js库将Markdown转换成HTML
 
 `
-let md = `
-# 标题1
+var md = `
+# 自我介绍
+
+  我叫王旭东
+
+# 技能介绍
+
+熟悉 JavaScript CSS jQuery ,懂得ajax json jsonp等.
+
+# 项目介绍
+
+ 1. XXX 轮播
+ 2. XXX 简历
+ 3. XXX 画板
 `
+
 writeCode('', result, () => {
+
   createPaper(() => {
-    console.log('paper有了')
-    writeCode(result, result2, ()=>{
-      writeMD(md)
+
+    writeCode(result, '', ()=>{
+
+      writeMD(md, ()=>{
+
+        writeCode(result, result2, ()=>{
+
+          convertMDtoHTML();
+        })
+      })
     })
   })
 })
+
+function writeCode(prefix, code, fn) {
+  let domCode = document.querySelector('#code');
+  let n = 0;
+  let id = setInterval(() => {
+    n += 1;
+    //将code的0到前n个字符高亮放到domCode中
+    domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+    //将code的0到n个字符写到style标签中
+    styleTag.innerHTML = prefix + code.substring(0, n);
+    domCode.scrollTop = domCode.scrollHeight; //如果页面过长,就自动下拉,最多10000 
+    if (n >= code.length) {
+      window.clearInterval(id)
+      fn()   //fn && fn.call()
+    }
+  }, 5);
+}
+
+function writeMD(md, fn) {
+  let domPaper = document.querySelector('#paper>.content')
+  let n = 0;
+  let id = setInterval(() => {
+    n += 1;
+    domPaper.innerHTML = md.substring(0, n);    //将code的0到前n个字符高亮放到domCode中
+    domPaper.scrollTop = domPaper.scrollHeight; //如果页面过长,就自动下拉,最多10000 
+    if (n >= md.length) {
+      window.clearInterval(id)
+      console.log(fn)
+      fn && fn()       //fn && fn.call()
+    }
+  }, 5);
+}
 
 
 function createPaper(fn) {
   let paper = document.createElement('div');
   paper.id = 'paper';
-  let content = document.createElement('div');
+  let content = document.createElement('pre');
   content.className = 'content'
   paper.appendChild(content)
   document.body.appendChild(paper)
   fn.call()    //fn && fn.call()
 }
+
+function convertMDtoHTML(){
+  let div = document.createElement('div');
+  div.className = 'markdown-body'
+  div.innerHTML = marked(md);
+  let mdContainer = document.querySelector('#paper> .content')
+  mdContainer.replaceWith(div);
+}
+
+//遇到的问题  
+//代码很长后不会自动向下滚动
+
+
